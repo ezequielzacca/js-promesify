f350b0992d69dcd7879849f7bf47f7568a091197
Ezequiel Zacca <ezacca@silentiumapps.com>
2019-06-21 12:05:43 -0300

    export functions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-function Promisefy<T, P1>(f: Function, param1: P1): Promise<T>;
-function Promisefy<T, P1, P2>(f: Function, param1: P1, param2: P2): Promise<T>;
-function Promisefy<T, P1, P2, P3>(f: Function, param1: P1, param2: P2, param3: P3): Promise<T>;
-function Promisefy<T>(f: Function, ...params: Array<any>): Promise<T> {
+export function Promisefy<T, P1>(f: Function, param1: P1): Promise<T>;
+export function Promisefy<T, P1, P2>(f: Function, param1: P1, param2: P2): Promise<T>;
+export function Promisefy<T, P1, P2, P3>(f: Function, param1: P1, param2: P2, param3: P3): Promise<T>;
+export function Promisefy<T>(f: Function, ...params: Array<any>): Promise<T> {
     return new Promise<T>((resolve, reject) => {
         f(...params, (err: any, res: any) => {
             if (err) {
