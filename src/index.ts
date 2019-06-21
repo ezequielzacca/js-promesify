ef6132820d1919ec60bb5fff305e7734e4808826
Ezequiel Zacca <ezacca@silentiumapps.com>
2019-06-21 12:12:15 -0300

    fix types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export function Promisefy<T, P1>(f: Function, param1: P1): Promise<T>;
+
+export function Promisefy<T>(f: Function, ...params: Array<any>): Promise<T>;
 export function Promisefy<T, P1, P2>(f: Function, param1: P1, param2: P2): Promise<T>;
 export function Promisefy<T, P1, P2, P3>(f: Function, param1: P1, param2: P2, param3: P3): Promise<T>;
 export function Promisefy<T>(f: Function, ...params: Array<any>): Promise<T> {
