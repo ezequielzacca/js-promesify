2d008accbb07bd2c6242de2f18b25a6db76fe883
Ezequiel Zacca <ezacca@silentiumapps.com>
2019-06-21 11:06:21 -0300

    first commit

---
+++ src/promesify.ts
@@ -0,0 +1,15 @@
+function Promisefy<T, P1>(f: Function, param1: P1): Promise<T>;
+function Promisefy<T, P1, P2>(f: Function, param1: P1, param2: P2): Promise<T>;
+function Promisefy<T, P1, P2, P3>(f: Function, param1: P1, param2: P2, param3: P3): Promise<T>;
+function Promisefy<T>(f: Function, ...params: Array<any>): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+        f(...params, (err: any, res: any) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(res);
+        })
+    })
+}
+
+
