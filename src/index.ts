67463d8c07c92fd975987e8b395d392317fc020e
Ezequiel Zacca <ezacca@silentiumapps.com>
2019-06-21 12:39:51 -0300

    single param variant

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 
 export function Promesify<T>(f: Function, ...params: Array<any>): Promise<T>;
+export function Promesify<T, P1>(f: Function, param1: P1): Promise<T>;
 export function Promesify<T, P1, P2>(f: Function, param1: P1, param2: P2): Promise<T>;
 export function Promesify<T, P1, P2, P3>(f: Function, param1: P1, param2: P2, param3: P3): Promise<T>;
 export function Promesify<T>(f: Function, ...params: Array<any>): Promise<T> {
